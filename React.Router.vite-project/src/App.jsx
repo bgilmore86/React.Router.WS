d1bdbf95d42b54d5bb8a3d27e65d753b2bbb8420
Barb T <stayyounng86@gmail.com>
2024-01-11 19:41:11 -0600

    created repo and added all React.Router.vite-project files

---
+++ React.Router.vite-project/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { Routes, Route, Link } from "react-router-dom"
+
+
+function App() {
+
+  return (
+  <div id="container">
+
+    <div id="navbar">
+      <Link to="/blue"></Link>
+      <Link to="/red"></Link>
+    </div>    
+    
+    <div id="main-section">
+      <Routes>
+        <Route path="/blue" element={<h1>Blue</h1>}/>
+        <Route path="/red" element={<h1>Red</h1>}/>
+      </Routes>
+    </div>
+
+  </div>
+  
+
+  );
+  
+}
+
+export default App
