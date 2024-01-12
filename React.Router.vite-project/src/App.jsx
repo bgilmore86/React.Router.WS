331516f13242d53e6561b586c3c5f5293333401e
Barb T <stayyounng86@gmail.com>
2024-01-12 01:14:32 -0600

    update 5

--- React.Router.vite-project/src/App.jsx
+++ React.Router.vite-project/src/App.jsx
@@ -1,21 +1,26 @@
 import './App.css'
+import './App.css'
 import { Routes, Route, Link } from "react-router-dom"
-
+import { Red } from "./components/Red.jsx"
+import { Blue } from "./components/Blue.jsx"
+import { Home } from "./components/Home.jsx"
 
 function App() {
 
-  return (
+return (
   <div id="container">
 
     <div id="navbar">
-      <Link to="/blue"></Link>
-      <Link to="/red"></Link>
+      <Link to="/blue">Blue</Link>
+      <Link to="/red">Red</Link>
+      <Link to="/">Home</Link>
     </div>    
     
     <div id="main-section">
       <Routes>
         <Route path="/blue" element={<h1>Blue</h1>}/>
         <Route path="/red" element={<h1>Red</h1>}/>
+        <Route path="/" element={<h1>Home</h1>}/>
       </Routes>
     </div>
 
--- React.Router.vite-project/src/components/Blue.jsx
+++ React.Router.vite-project/src/components/Blue.jsx
@@ -1,7 +1,7 @@
 export default function Blue() {
     return (
         <div className="blue">
-            <h1>BLUE</h1>
+            <h1>Blue</h1>
         </div>
     );
 }
